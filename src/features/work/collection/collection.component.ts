3cd4a27506ab97ff9ff5e02a2af64cd9e82eeb87
Armando Villanueva <armandojvilla9@gmail.com>
2024-09-07 07:21:31 -0400

    collection

--- src/features/work/collection/collection.component.ts
+++ src/features/work/collection/collection.component.ts
@@ -8,7 +8,32 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./collection.component.scss']
 })
 export class CollectionComponent implements OnInit {
-  data: ProjectInterface[] = [];
+  data: ProjectInterface[] = [
+    {
+      id: 0,
+      title: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
+      skills: ["Chinook Salmon", "Aardvark", "Raven", "Gecko", "Nubian Ibex", "Echidna"],
+      media: ["https://picsum.photos/1600", "https://picsum.photos/1601/900", "https://picsum.photos/1200/800"]
+    },
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
+      skills: ["Chinook Salmon", "Aardvark", "Raven", "Gecko", "Nubian Ibex", "Echidna"],
+      media: ["https://picsum.photos/1700", "https://picsum.photos/1600/1000", "https://picsum.photos/1200/1500"]
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ",
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
+      skills: ["Chinook Salmon", "Aardvark", "Raven", "Gecko", "Nubian Ibex", "Echidna"],
+      media: ["https://picsum.photos/1500", "https://picsum.photos/600/900", "https://picsum.photos/1300"]
+    }
+  ];
 
   public skillOptions = ['python', 'php', 'laravel'];
 
