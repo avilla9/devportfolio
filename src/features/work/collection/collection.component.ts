2c054a9f6bc3b29e6f37106a2877b7daf4e2783b
Armando Villanueva <armandojvilla9@gmail.com>
2024-09-07 07:23:44 -0400

    collections

--- src/features/work/collection/collection.component.ts
+++ src/features/work/collection/collection.component.ts
@@ -19,7 +19,7 @@ export class CollectionComponent implements OnInit {
     },
     {
       id: 1,
-      title: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      title: "consectetuer adipiscing elit.",
       summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ",
       description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
       skills: ["Chinook Salmon", "Aardvark", "Raven", "Gecko", "Nubian Ibex", "Echidna"],
@@ -27,7 +27,7 @@ export class CollectionComponent implements OnInit {
     },
     {
       id: 2,
-      title: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      title: "dolor sit amet elit.",
       summary: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ",
       description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum ",
       skills: ["Chinook Salmon", "Aardvark", "Raven", "Gecko", "Nubian Ibex", "Echidna"],
